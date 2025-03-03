b29dc50f6f9b4211a9ebb20c5060029f7165b5c2
elvis <elvisotieno24@gmail.com>
2025-03-03 10:59:32 +0300

    adds changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// script.js
 document.getElementById('changecolor').addEventListener('click', function() {
-    document.body.style.backgroundColor ='brown'
+    const screenWidth = window.innerWidth;
+    let colors;
+
+    if (screenWidth >= 750) {
+        // Large screens (desktops)
+        colors = ['#FF5733', '#33FF57', '#3357FF'];
+    } else if (screenWidth >= 500) {
+        // Medium screens (tablets)
+        colors = ['#FFC300', '#900C3F', '#581845',];
+    } else {
+        // Small screens (mobile devices)
+        colors = ['#C70039', '#FF5733', '#900C3F'];
+    }
+
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
 });
